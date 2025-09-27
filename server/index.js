0af294a3b5529bbe851e19615a69676eb7c0dbd4
Ashfar-05092005 <mohammedashfar05092005@gmail.com>
2025-09-27 11:16:20 +0530

    $message

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const fetch = require("node-fetch"); // if using Node <18
 
 const app = express();
 
@@ -44,8 +45,8 @@ app.post("/api/summarize", async (req, res) => {
 // Serve React frontend (production)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
